d3e1936f5eaf55c9de8fbd0380febdc1c4c153af
s.shakhbazyan <s.shakhbazyan@kt-team.de>
2025-07-22 10:01:10 +0400

    добавление /showcase

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/showcase" element={<LandingPage />} />
       </Routes>
     </Router>
   );
