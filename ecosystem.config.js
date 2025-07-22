72e8c824c923712ad8c9734876a74ee6cd9d722c
s.shakhbazyan <s.shakhbazyan@kt-team.de>
2025-07-22 09:32:32 +0400

    добавление инструкций

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,12 @@
-module.exports = {
+export default {
   apps: [
     {
       name: 'giftassets',
       script: 'npm',
       args: 'run preview',
       cwd: './',
-      instances: 1,
+      instances: 'max',
+      exec_mode: 'cluster',
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
@@ -16,7 +17,12 @@ module.exports = {
       env_production: {
         NODE_ENV: 'production',
         PORT: 4173
-      }
+      },
+      interpreter: 'none',
+      error_file: './logs/err.log',
+      out_file: './logs/out.log',
+      log_file: './logs/combined.log',
+      time: true
     }
   ]
 }; 
